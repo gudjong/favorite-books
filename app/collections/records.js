465d9276ae18d92f71c474cab6bf71dfb38adf62
gudjon <gudjon@sprettur.is>
2014-03-12 22:25:02 +0000

    Added schema for records.

--- app/collections/records.js
+++ app/collections/records.js
@@ -1,1 +1,28 @@
-Records = new Meteor.Collection('records');
+Records = new Meteor.Collection('records', {
+    schema: new SimpleSchema({
+        transactionId: {
+            type: String,
+            denyUpdate: true
+        },
+        account: {
+            type: String,
+            denyUpdate: true
+        },
+        debit: {
+            type: Number,
+            optional: true,
+            min: 0,
+            denyUpdate: true
+        },
+        kredit: {
+            type: Number,
+            optional: true,
+            min: 0,
+            denyUpdate: true
+        },
+        description: {
+            type: String,
+            denyUpdate: true
+        }
+    })
+});
